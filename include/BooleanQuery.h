4ca5970000dad49d4f502b0c6b4531b943e14589
Zaid Mayyalleh <164514360+zaidmoen@users.noreply.github.com>
2026-09-16 16:54:17 +0300

    refactor: simplify boolean query types

--- include/BooleanQuery.h
+++ include/BooleanQuery.h
@@ -5,17 +5,18 @@
 #include <set>
 #include <string>
 
+using namespace std;
+
 class BooleanQuery {
 public:
     BooleanQuery(const InvertedIndex& index, int documentCount);
-    std::set<int> evaluate(const std::string& query) const;
+    set<int> evaluate(const string& query) const;
 
 private:
-    std::set<int> documentsForTerm(const std::string& term) const;
-    std::set<int> allDocuments() const;
-    static std::vector<std::string> split(const std::string& query);
+    set<int> documentsForTerm(const string& term) const;
+    set<int> allDocuments() const;
+    static vector<string> split(const string& query);
 
     const InvertedIndex& index;
     int documentCount;
 };
-
